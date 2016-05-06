2064265696ae7db1e3a3384f301a882909ca580d
Chris Nordhougen <cwnord@gmail.com>
2016-05-06 15:56:22 -0500

    Removed extraneous console.log >_>

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ let extdocs    = {},
 try {
     extdocs = require('./external-docs.json');
     customdocs = require('../../external-docs.json');
-} catch (e) { console.error(e); }
+} catch (e) {}
 
 Object.assign(extdocs, customdocs);
 
 exports.handlers = {
     parseBegin() {
-        console.log(extdocs);
         for (const key of Object.keys(extdocs)) {
             registerLink(key, extdocs[key]);
         }
